4b4d8fed2bdd9089d0034159d7fe42d67b627af1
ruchira-cr <dammikaranawaka116@gmail.com>
2026-05-13 17:57:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/ruchira-cr/Ruchira-/blob/main/images/file_00000000bfdc71fda876be8da4093e46.png",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 RUCHIRA-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
